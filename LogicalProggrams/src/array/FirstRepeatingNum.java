f8c32c5e49c2769db8bfbc1f7524cf474576db39
HimanshuM123 <himanshu.mahale2010@gmail.com>
2023-08-15 15:29:26 -0400

    Aug commits

---
+++ LogicalProggrams/src/array/FirstRepeatingNum.java
@@ -0,0 +1,31 @@
+package array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FirstRepeatingNum {
+	public static void main(String[] args) {
+		int array[] = { 10, 7, 8, 1, 8, 7, 6 };
+		int min = getFirstRepeatingElementArray(array);
+		if (min == -1) {
+			System.out.println("No element found");
+		} else {
+			System.out.println("The first repeating element is " + min);
+		}
+	}
+
+	private static int getFirstRepeatingElementArray(int[] arr) {
+
+		Set<Integer> set = new HashSet<>();
+		int element = -1;
+
+		for (int i = arr.length - 1; i > 0; i--) {
+			if (set.contains(arr[i])) {
+				element = arr[i];
+			} else {
+				set.add(arr[i]);
+			}
+		}
+		return element;
+	}
+}
--- LogicalProggrams/src/leetcode2/LinkedListPalimdrom.java
+++ LogicalProggrams/src/leetcode2/LinkedListPalimdrom.java
@@ -30,14 +30,50 @@ public class LinkedListPalimdrom {
 			node = node.next;
 		}
 	}
+	//10->20->30->20->10
+	private static boolean isPalimdrome(Node head) {
+		Node slow = head;
+		Node fast = head;
+		while(fast != null && fast.next!=null) {
+			fast = fast.next.next;//10-> null
+			slow = slow.next;// 30 ->20->10
+		}
+		slow=reverse(slow);
+		while(slow !=null) {
+			if (slow.data !=head.data) {
+				return false;
+			}
+			slow=slow.next;
+			head=head.next;
+		}
+		return true;
+	}
+	
+	private  static Node reverse(Node head) {
+		Node prev =null;
+		while(head !=null) {
+			Node remaining = head.next; //20->30->40
+			head.next = prev; //10->null
+			prev = head;    //10->null
+			head = remaining; // 20->30->40
+		}
+		return prev;
+	}
 
 	public static void main(String[] args) {
 		LinkedListPalimdrom ll = new LinkedListPalimdrom();
 		ll.addLast(10);
 		ll.addLast(20);
 		ll.addLast(30);
-		ll.printList(ll);
-	
+		ll.addLast(20);
+		ll.addLast(10);
+		//ll.printList(ll);
+	    boolean val = isPalimdrome(ll.first);
+	    if(val) {
+	    	System.out.println("The linkedlist is palimdrom");
+	    }else {
+	    	System.out.println("The linkedlist is NOT palimdrom");
+	    }
 	}
 
 }
