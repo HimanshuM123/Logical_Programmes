e34e01887503372b74aefee041b3427dab646b15
HimanshuM123 <himanshu.mahale2010@gmail.com>
2023-09-07 23:40:29 -0400

    sept 7

---
+++ LogicalProggrams/src/dataStructure/LinkedListDeleteHead.java
@@ -0,0 +1,49 @@
+package dataStructure;
+
+public class LinkedListDeleteHead {
+	Node first;
+	Node last;
+	private class Node {
+		int data;
+		Node next;
+		
+		Node(int data){
+			this.data=data;
+		}
+	}
+	
+	private void addLast(int n) {
+		Node node = new Node(n);
+		if(first == null) {
+			first = last = node;
+		}else {
+			last.next = node;
+			last = node;
+		}		
+	}
+	
+	private void print(Node head) {
+		while(head != null) {
+			System.out.print(head.data+" ");
+			head = head.next;
+		}
+	}
+	
+	private Node deleteHead(Node head) {
+		head=head.next;
+		return head;
+		
+	}
+	
+	public static void main(String[] args) {
+		LinkedListDeleteHead ll = new LinkedListDeleteHead();
+		ll.addLast(10);
+		ll.addLast(20);
+		ll.addLast(30);
+		//ll.print(ll.first);
+		Node n =ll.deleteHead(ll.first);
+		ll.print(n);
+		
+	}
+
+}
---
+++ LogicalProggrams/src/dataStructure/LinkedListDeleteNode.java
@@ -0,0 +1,62 @@
+package dataStructure;
+
+public class LinkedListDeleteNode {
+	Node first;
+	Node last;
+
+	private class Node {
+		int data;
+		Node next;
+
+		Node(int data) {
+			this.data = data;
+		}
+	}
+
+	private void addLast(int d) {
+		Node node = new Node(d);
+		if (first == null) {
+			first = last = node;
+		} else {
+			last.next = node;
+			last = node;
+		}
+	}
+
+	private void print(Node head) {
+		while (head != null) {
+			System.out.print(head.data + "->");
+			head = head.next;
+		}
+	}
+
+	private boolean deleteNode(Node head, int d) {
+		while (head.next != null) {
+			if (head.next.data == d) {
+				head.next = head.next.next;
+				return true;
+			}
+			head = head.next;
+		}
+		return false;
+
+	}
+
+	public static void main(String[] args) {
+		LinkedListDeleteNode ll = new LinkedListDeleteNode();
+		ll.addLast(10);
+		ll.addLast(20);
+		ll.addLast(30);
+		ll.addLast(40);
+		ll.addLast(50);
+		 ll.print(ll.first);
+		 System.out.println();
+		 ll.deleteNode(ll.first, 30);
+		ll.print(ll.first);
+	}
+	/*
+	10->20->30->40->50
+	      |    |
+	       --->
+*/
+}
