8d4eba599d8041830518be7656ccafebe6b0968f
HimanshuM123 <himanshu.mahale2010@gmail.com>
2023-11-15 15:14:47 +0530

    15th Nov

---
+++ LogicalProggrams/src/heap/LargestTripletProduct.java
@@ -0,0 +1,32 @@
+package heap;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class LargestTripletProduct {
+	private  static void largestTripletMultiplication(int arr[], int n) {
+		PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+		
+		for(int i=0;i<n ;i++) {
+			queue.add(arr[i]);
+			if(queue.size()<3) {
+				System.out.println(-1);
+			}else {
+				int x = queue.poll();
+				int y = queue.poll();
+				int z = queue.poll();
+				int ans = x*y*z;
+				System.out.println(ans);
+				queue.add(x);
+				queue.add(y);
+				queue.add(z);
+				
+			}
+		}
+	}
+	public static void main(String[] args) {
+		int arr[] = { 1, 2, 3, 4, 5 };
+        int n = arr.length;
+        largestTripletMultiplication(arr, n);
+	}
+}
