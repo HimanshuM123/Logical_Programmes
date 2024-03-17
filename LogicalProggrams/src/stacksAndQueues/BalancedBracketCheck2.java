0c03bb4ea09a169e691a437fea4840f495330617
HimanshuM123 <himanshu.mahale2010@gmail.com>
2024-03-17 10:53:48 -0400

    17th March

--- LogicalProggrams/src/stacksAndQueues/BalancedBracketCheck2.java
+++ LogicalProggrams/src/stacksAndQueues/BalancedBracketCheck2.java
@@ -1,28 +1,13 @@
-package practice2;
+package stacksAndQueues;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Set;
 import java.util.Stack;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
 
-import binaryTree.BinaryTreeMirror;
-import binaryTree.BinaryTreeSum;
-import leetcode2.LinkedListPalimdrom;
-
-public class Practice2 {
-
+public class BalancedBracketCheck2 {
 	public static void main(String[] args) {
 
-		String str = "[{(hh)})";
+		String str = "[{((hh)}]";
 
 		boolean flag = balancedBracket(str);
 		if (flag) {
@@ -30,32 +15,31 @@ public class Practice2 {
 		} else {
 			System.out.println("No balanced brackets");
 		}
-
 	}
 
 	private static boolean balancedBracket(String str) {
 
 		Stack<Character> stk = new Stack<>();
-		Map<Character,Character> hm = new HashMap<>();
+		Map<Character, Character> hm = new HashMap<>();
 		hm.put('(', ')');
 		hm.put('[', ']');
 		hm.put('{', '}');
-		
+		boolean isExtraChar = false;
 		for (int i = 0; i < str.length(); i++) {
 			char ch = str.charAt(i);
-
 			if (hm.containsKey(ch)) {
 				stk.push(ch);
 			}
-			if(hm.containsValue(ch)) {
+			if (hm.containsValue(ch) && !stk.isEmpty()) {
 				char a = stk.pop();
-				if(hm.get(a)!= ch) {
+				if (hm.get(a) != ch) {
 					return false;
 				}
+			} else {
+				isExtraChar = true;
 			}
-		
 		}
-		return stk.isEmpty();
+		return stk.isEmpty() && !isExtraChar;
 
 	}
 }
