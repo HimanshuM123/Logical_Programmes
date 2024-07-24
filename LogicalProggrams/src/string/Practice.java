3098a5dca78ff8851d582ef62f1165ef007cb9a3
HimanshuM123 <himanshu.mahale2010@gmail.com>
2024-07-23 22:58:17 -0400

    String

--- LogicalProggrams/src/string/Practice.java
+++ LogicalProggrams/src/string/Practice.java
@@ -1,25 +1,33 @@
 package string;
 
+import java.util.Arrays;
+
 public class Practice {
-	public static void main(String[] args) {
-		
-		String str = "Hello World56   ";
-		int j=str.length()-1;
-		int count=0;
-		
-		while(str.charAt(j)==' ') {
-			j--;
-		}
-		
-		
-		while(str.charAt(j)!=' ') {
-			count++;
-			j--;
-		}
-		System.out.println(count);
-		
-		
-		
+	 public static void main(String[] args) {
+		 String str ="loveleetcode";
+			int index = firstUniqChar(str);
+			System.out.println(index);
 	}
+	 
+	 private static int firstUniqChar(String str) {
+		 int index =0;
+		 
+		 int [] freq = new int[26];
+		 Arrays.fill(freq, -1);
+		 
+		 for(Character ch : str.toCharArray()) {
+			 freq[ch-'a']++;
+		 }
+		 
+		 for(int i=0; i<str.length(); i++){
+	            if(freq[str.charAt(i)-'a']==0){
+	                return i;
+	            }
+	        }
+		 
+		 
+		 
+		 return -1;
+	 }
 
 }
