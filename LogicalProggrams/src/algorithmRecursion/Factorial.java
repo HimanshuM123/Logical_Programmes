da0500a6a26f6732945b4a7a343ff5fb66f125dc
HimanshuM123 <himanshu.mahale2010@gmail.com>
2024-08-05 21:14:33 -0400

    recursion

--- LogicalProggrams/src/algorithmRecursion/Factorial.java
+++ LogicalProggrams/src/algorithmRecursion/Factorial.java
@@ -2,15 +2,15 @@ package algorithmRecursion;
 
 public class Factorial {
 	public static void main(String[] args) {
-		System.out.println(len("Himanshu"));
+		System.out.println(fact(5));
 	}
 	
-	public  static int len(String str) {
-		if(str.equals("")) {
-			return 0;
+	public  static int fact(int num) {
+		if(num ==0) {
+			return 1;
 		}
 		
-		return 1+ len(str.substring(1));
+		return num *fact(num-1);
 	}
 
 }
