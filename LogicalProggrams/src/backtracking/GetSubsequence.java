626b91278e59ab697193a483ad86b075af6d0751
HimanshuM123 <himanshu.mahale2010@gmail.com>
2024-08-18 22:43:45 -0400

    backtracking

---
+++ LogicalProggrams/src/backtracking/GetSubsequence.java
@@ -0,0 +1,58 @@
+package backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class GetSubsequence {
+	
+	public static void main(String[] args) {
+		String str ="abc";
+		
+		ArrayList<String> res = getSubsequence(str);
+		System.out.println(res);
+	}
+	
+	private static ArrayList<String> getSubsequence(String str){
+		if(str.length()==0) {
+			ArrayList<String> blank = new ArrayList<>();
+			blank.add("");
+			return blank;
+		}
+		
+		char ch = str.charAt(0);
+		String rest = str.substring(1);
+		ArrayList<String> list = getSubsequence(rest);
+		
+		ArrayList<String> result = new ArrayList<>();
+		for(String obj :list) {
+			result.add(""+obj);
+		}
+		for(String obj :list) {
+			result.add(ch+obj);
+		}
+		return result;
+		
+	}
+
+}
+
+
+/*
+abc
+- +E(bc)
+a +E(bc)
+
+  ---
+  --c
+  -b-
+  -bc
+  a--
+  a-c
+  ab-
+  abc
+  
+
+
+
+*/
