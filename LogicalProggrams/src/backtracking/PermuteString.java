ce36266e1c699883b8efcf2a3e1987a0af7ed919
HimanshuM123 <himanshu.mahale2010@gmail.com>
2024-09-14 22:36:38 -0400

    test

--- LogicalProggrams/src/backtracking/PermuteString.java
+++ LogicalProggrams/src/backtracking/PermuteString.java
@@ -5,30 +5,23 @@ import java.util.List;
 
 public class PermuteString {
 	public static void main(String[] args) {
-		String str = "ABC";
-//		List<String> list = new ArrayList<>();
-//		List<String> result = new ArrayList<>();
-//		PermuteString obj = new PermuteString();
-//		obj.permuteString(str,result,list);
-//		System.out.println(result);
-		
-	}
-	
-//	private void permuteString(String str, List<String> result, List<String> list) {
-//		if (list.size() == result.size()) {
-//			result.addAll(list);
-//			return ;
-//		}
-//
-//		for (Character ch : str.toCharArray()) {
-//			if (!list.contains(ch + "")) {
-//				list.add(ch + "");
-//				permuteString(str, result, list);
-//				list.remove(list.size() - 1);
-//			}
-//
-//		}
-//
-//	}
+		String str = "abc";
+		permuteString(str, "");
 
+	}
+
+	private static void permuteString(String str, String asf) {
+		if (str.length() == 0) {
+			System.out.print(asf + " ");
+		}
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			String left = str.substring(0, i);
+			String right = str.substring(i + 1);
+			String result = left + right;
+			permuteString(result, asf + ch);
+		}
+
+	}
 }
--- LogicalProggrams/src/number/Practice.java
+++ LogicalProggrams/src/number/Practice.java
@@ -1,28 +1,36 @@
 package number;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Practice {
 
 	public static void main(String[] args) {
+
+	int num =54123;
+	
+	
+	
+	List<String> list = Arrays.asList("5","4","1","2","3");
+	
+	List<String> result  =list.stream().sorted((a,b)-> a.compareTo(b)).collect(Collectors.toList());
+	System.out.println(result);
+	
+	List<Integer> list2 = Arrays.asList(5,4,1,2,3);
+	
+	List<Integer> result2  =list2.stream().sorted((a,b)-> a-b).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+	System.out.println(result2);
 		
-		for (int i = 0; i < 20; i++) {
-			int res = fibo(i);
-			System.out.println(res);
-		}
-
 	}
-
-	private static int fibo(int num) {
-		if (num <= 1) {
-			return 1;
-		}
-
-		return fibo(num - 1) + fibo(num - 2);
-
-	}
-
+	
 }
 
 // 1 1 2 3 5 8 13 21 34 55
