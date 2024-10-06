2cf6c1d3f64eaab904783c744ded8eedb57b9c7c
HimanshuM123 <himanshu.mahale2010@gmail.com>
2024-10-06 19:51:30 -0400

    test

--- LogicalProggrams/src/myPkg/DuplicateCharsInString.java
+++ LogicalProggrams/src/myPkg/DuplicateCharsInString.java
@@ -9,6 +9,7 @@ public class DuplicateCharsInString {
 		String myString="andamaan";
 		char[] arr=myString.toCharArray();
 		Map<Character,Integer> hm=new HashMap<>();
+		Map<Character,Integer> hm2=new HashMap<>();
 		
 		for(char obj:arr) {
 			//if(hm.containsKey(obj)) {
@@ -25,5 +26,11 @@ public class DuplicateCharsInString {
 				System.out.print(k+"  ");
 			}
 		});
+		
+		
+		for (char obj : arr) {
+			hm2.merge(obj, 1, Integer::sum);
+		}
+		System.out.println(hm2);// {a=4, d=1, m=1, n=2}
 	}
 }
