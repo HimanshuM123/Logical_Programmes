033aec3c716a82588062a41bad708a60cb1ab928
HimanshuM123 <himanshu.mahale2010@gmail.com>
2024-12-03 16:29:45 -0500

    test

--- LogicalProggrams/src/array/RemoveDuplicatesFromSortedArray.java
+++ LogicalProggrams/src/array/RemoveDuplicatesFromSortedArray.java
@@ -1,23 +1,30 @@
 package array;
 
 public class RemoveDuplicatesFromSortedArray {
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
 
-	public static void main(String[] args) {
-		int[] array = { 1, 1, 2, 2, 3, 4, 5, 5, 5 };
+        int uniqueIndex = 0; // This will track the position of unique elements.
 
-		int length = removeDuplicates(array);
-		System.out.println("Length after removing duplicates: " + length);
-	}
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[uniqueIndex]) { // Check for a new unique value.
+                uniqueIndex++; // Move the unique index forward.
+                nums[uniqueIndex] = nums[i]; // Assign the new unique value.
+            }
+        }
 
-	private static int removeDuplicates(int[] array) {
-		int index = 1;
-		for (int i = 1; i < array.length; i++) {
-			if (array[i - 1] != array[i]) {
-				index++;
-			}
-		}
-		return index;
+        return uniqueIndex + 1; // Length of the unique elements.
+    }
 
-	}
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2, 2,2, 3, 4, 4, 5};
+        int length = removeDuplicates(nums);
 
+        System.out.println("Array after removing duplicates:");
+        for (int i = 0; i < length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
 }
